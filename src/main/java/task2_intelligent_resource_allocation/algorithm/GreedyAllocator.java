dfb9572b887cfc345154bbca1cd1c5c7fd8bb222
Sahas Suraweera <sahassuraweera11@gmail.com>
2026-09-03 08:48:54 +0530

    Updated algorithm

--- src/main/java/task2_intelligent_resource_allocation/algorithm/GreedyAllocator.java
+++ src/main/java/task2_intelligent_resource_allocation/algorithm/GreedyAllocator.java
@@ -1,8 +1,5 @@
 package task2_intelligent_resource_allocation.algorithm;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
 public class GreedyAllocator {
 
     public static class CandidateVehicle {
@@ -16,12 +13,59 @@ public class GreedyAllocator {
     }
 
     public static CandidateVehicle findBestVehicle(long[] vehicleIds, double[] distances) {
-        PriorityQueue<CandidateVehicle> minHeap = new PriorityQueue<>(Comparator.comparingDouble(v -> v.transitCostKm));
+        int n = vehicleIds.length;
+        if (n == 0) return null;
 
-        for (int i = 0; i < vehicleIds.length; i++) {
-            minHeap.add(new CandidateVehicle(vehicleIds[i], distances[i]));
+        CandidateVehicle[] heap = new CandidateVehicle[n];
+        int size = 0;
+
+        for (int i = 0; i < n; i++) {
+            CandidateVehicle vehicle = new CandidateVehicle(vehicleIds[i], distances[i]);
+            heap[size] = vehicle;
+            int current = size;
+            size++;
+
+            while (current > 0) {
+                int parent = (current - 1) / 2;
+                if (heap[current].transitCostKm < heap[parent].transitCostKm) {
+                    CandidateVehicle temp = heap[current];
+                    heap[current] = heap[parent];
+                    heap[parent] = temp;
+                    current = parent;
+                } else {
+                    break;
+                }
+            }
         }
 
-        return minHeap.poll();
+        CandidateVehicle min = heap[0];
+
+        heap[0] = heap[size - 1];
+        size--;
+
+        int index = 0;
+        while (true) {
+            int smallest = index;
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+
+            if (left < size && heap[left].transitCostKm < heap[smallest].transitCostKm) {
+                smallest = left;
+            }
+            if (right < size && heap[right].transitCostKm < heap[smallest].transitCostKm) {
+                smallest = right;
+            }
+
+            if (smallest != index) {
+                CandidateVehicle temp = heap[index];
+                heap[index] = heap[smallest];
+                heap[smallest] = temp;
+                index = smallest;
+            } else {
+                break;
+            }
+        }
+
+        return min;
     }
 }
